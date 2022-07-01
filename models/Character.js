4be2f0dc424683665566026963c822646d3e2d72
Ian Erickson <Ian@micrognomics.com>
2022-07-01 19:15:13 -0400

    Added ObjectId to character.

--- models/Character.js
+++ models/Character.js
@@ -1,6 +1,7 @@
 'use strict';
 const mongoose = require( 'mongoose' );
 const Schema = mongoose.Schema;
+const ObjectId = Schema.Types.ObjectId;
 
 var characterSchema = Schema( {
   userId: {type:Schema.Types.ObjectId, ref:'User'},
